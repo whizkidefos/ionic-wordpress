fc1d4ee7ff3bc58b043446aa93f1d9b1f5d0cd86
whizkidefos <liova2273@gmail.com>
2019-07-05 09:37:38 +0200

    First commit after scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'posts', loadChildren: './pages/posts/posts.module#PostsPageModule' },
+  { path: 'post', loadChildren: './pages/post/post.module#PostPageModule' },
 ];
 
 @NgModule({
